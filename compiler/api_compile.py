eda3caefbad6e88431eef00c924f527f07096cdd
Leo dev <klesti.selimay@icloud.com>
2024-01-24 21:25:38 +0100

    compiler

--- compiler/api_compile.py
+++
@@ -1,8 +0,0 @@
-import compiler_prismscript
-import json
-import sys
-
-while True:
-    x = compiler_prismscript.compile(sys.stdin.readline().replace('\\n', '\n'))
-    print(json.dumps({'error': x[1], 'output': x[0]}))
-    sys.stdout.flush()
